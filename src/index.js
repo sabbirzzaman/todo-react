431779a325706a4b92b5cf16d57346eed7edec56
sabbirzzaman <sztonmoy7@gmail.com>
2022-05-19 02:34:21 +0600

    readme updated

--- src/index.js
+++ src/index.js
@@ -3,20 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {
-    QueryClient,
-    QueryClientProvider,
-} from 'react-query';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const queryClient = new QueryClient();
-
 root.render(
     <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <App />
-        </QueryClientProvider>
+        <App />
     </React.StrictMode>
 );
 
